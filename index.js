03cc4c832e49a74f837b6746a1cd5e369946b8bc
Caio <caionunes2801@gmail.com>
2022-06-09 20:37:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let nome = 'Caio Nunes'
+let idade = 24
+let ano = 2022 - idade
+let soma = idade + ano
+let templateString = 'Estou mexendo no console.log'
+let quartaFeira = true
+
+console.log(soma)
+console.log(templateString)
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
